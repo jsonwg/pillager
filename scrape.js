891ca5c3ce01bd4b41a6ce3caf8ffdb26c25b1aa
jason <jason@gateway.localdomain>
2021-07-31 13:01:58 -0700

    Add detection for existing AMQ page

--- scrape.js
+++ scrape.js
@@ -1,6 +1,20 @@
 import puppeteer from "puppeteer";
 import { PythonShell } from "python-shell";
 
+async function detectAMQ(browser) {
+  const pages = await browser.pages();
+  for (const page of pages) {
+    const url = page.url();
+    const isHidden = await page.evaluate(() => document.hidden);
+    if (!isHidden && url == "https://animemusicquiz.com/") {
+      return page;
+    }
+  }
+  const page = await browser.newPage();
+  await page.goto("https://animemusicquiz.com/");
+  return page;
+}
+
 async function saveSong(song) {
   const opts = { args: [song[0], song[1], song[2], song[3], song[4]] };
   PythonShell.run("write.py", opts, (err) => {
@@ -18,15 +32,13 @@ async function makeSong(page) {
 }
 
 (async () => {
-  const browserURL = "http://127.0.0.1:4444";
   const browser = await puppeteer.connect({
-    browserURL,
+    browserURL: "http://127.0.0.1:4444",
     defaultViewport: null,
   });
 
-  const page = await browser.newPage();
+  const page = await detectAMQ(browser);
   page.setDefaultTimeout(0);
-  await page.goto("https://animemusicquiz.com/");
 
   while (true) {
     await page.waitForXPath(
