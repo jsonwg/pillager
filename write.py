235863a54383e074d47a23bd6980b9fe0438d7de
jason <jason@gateway.localdomain>
2021-07-31 00:02:27 -0700

    Add fix for encoding issue

--- write.py
+++ write.py
@@ -4,7 +4,7 @@ FIELDS = ["Anime", "Song", "Artist", "Song Type", "Link", "Time"]
 
 first_write = True if not os.path.isfile("output.csv") else False
 
-with open("output.csv", "a") as data:
+with open("output.csv", "a", encoding="utf-8") as data:
     file = csv.writer(data)
     if first_write:
         file.writerow(FIELDS)
