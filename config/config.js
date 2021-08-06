e8a6e223717f165740317bad76c4d5e09e0e737c
Jason <ja2415n@gmail.com>
2021-08-06 01:09:01 -0700

    Change env variables to work with windows

--- config/config.js
+++ config/config.js
@@ -5,13 +5,10 @@ dotenv.config();
 
 let config = {};
 
-config.username = process.env.USERNAME;
-config.password = process.env.PASSWORD;
+config.username = process.env.AMQUSER;
+config.password = process.env.AMQPASS;
 config.settings =
   '6082s1111111132s000011110000002s1111005051o1100k012r02i0a46511002s0111110111002s0111002s01a111111111102a1111111111i01k503-11111--';
-
-config.key = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-config.sheet_id = process.env.SHEET_ID;
 
 const KEY = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const SHEET_ID = process.env.SHEET_ID;
